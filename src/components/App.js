2bd71672601c5c830c649c855c50d7ef64d18f94
ChrisRDavila <crossdavila@gmail.com>
2023-12-07 16:13:37 -0800

    moved style div around from index.js to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,25 @@ import React from 'react';
 import NavBar from './Header.js';
 import CartControl from './CartControl.js';
 import '../App.css';
+import background from '../img/bGround.jpg';
 
 
 function App(){
+  const defaultStyle = {
+    backgroundImage: `url(${background})`,
+    color: "#FFFC00",
+    fontFamily: ['Deborah Fancy Dress', "sans-serif"],
+    fontSize: "150%"
+  
+  }
   
   return ( 
     <React.Fragment>
-      <NavBar />
-      <CartControl />
+      <link href="https://fonts.cdnfonts.com/css/deborah-fancy-dress" rel="stylesheet"></link>
+      <div className="background" style={defaultStyle}>
+        <NavBar />
+        <CartControl />
+      </div>
     </React.Fragment>
   );
 }
--- src/components/OrderDetail.js
+++ src/components/OrderDetail.js
@@ -5,6 +5,15 @@ import shirt from "../img/Tshirt.png";
 import button from "../img/button.jpg";
 
 function OrderDetail(props){
+  
+  const itemStyling = {
+    textAlign: "left",
+    marginLeft: "30%",
+    marginRight: "30%",
+    borderStyle: "solid",
+    padding: "10px"
+  };
+  
   let input;
   const { order, onClickingDelete } = props;
   if (order.item === "album") {
@@ -14,17 +23,18 @@ function OrderDetail(props){
   } else if (order.item === "button") {
     input = button;
   }
-
   const altAttribute = `image of ${input}`;
   return (
     <React.Fragment>
-      <h1>Order Detail</h1>
-      <p>Quantity: {order.quantity}</p>
-      <p>Item: {order.item}</p>
-      <img src={input} alt= {altAttribute} />
-      <p>Description: {order.description}</p>
-      <button onClick={props.onClickingEdit}>Update Order</button>
-      <button onClick={()=> onClickingDelete(order.id) }>Close Order</button>
+      <h3>Order Detail</h3>
+      <div style={itemStyling}>
+        <p>Quantity: {order.quantity}</p>
+        <p>Item: {order.item}</p>
+        <img src={input} alt= {altAttribute} />
+        <p>Description: {order.description}</p>
+        <button onClick={props.onClickingEdit}>Update Order</button>
+        <button onClick={()=> onClickingDelete(order.id) }>Close Order</button>
+      </div>
       <hr/>
       <button onClick={props.onClickingBackToCart}>Return to Cart</button>
     </React.Fragment>
